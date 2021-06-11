df1ac4278e531eb45aace3b2ad22e5dce1c508b0
John-D-S <68535837+John-D-S@users.noreply.github.com>
2021-06-11 16:12:33 +1000

    added lava

--- Assets/Gooble Lump/Scripts/Lava.cs
+++ Assets/Gooble Lump/Scripts/Lava.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using static StaticObjectHolder;
 
 public class Lava : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float lavaRiseSpeed;
+
+    private void FixedUpdate()
     {
-        
+        float currentHeight = gameObject.transform.position.y;
+        gameObject.transform.position = new Vector3(player.AveragePosition.x, currentHeight + lavaRiseSpeed * Time.fixedDeltaTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        
+        if (collision.collider.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Main", LoadSceneMode.Single);
+        }
     }
 }
