ecea071f81ccd89ce5bf66af9af287bb5e6126ee
John-D-S <johnduncanstevenson@gmail.com>
2021-06-16 15:46:08 +1000

    added fancy ball shading

---
+++ Assets/Gooble Lump/Scripts/MaintainRotation.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaintainRotation : MonoBehaviour
+{
+    void Update()
+    {
+        gameObject.transform.rotation = Quaternion.identity;
+    }
+}
