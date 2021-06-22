8775dec20cdcd6b61a4e700681b296b5b8f2ca4c
John <68535837+John-D-S@users.noreply.github.com>
2021-06-22 11:20:38 +1000

    fixed menu toggles

--- Assets/Gooble Lump/Scripts/MakeNoiseOnCollision.cs
+++ Assets/Gooble Lump/Scripts/MakeNoiseOnCollision.cs
@@ -13,11 +13,13 @@ public class MakeNoiseOnCollision : MonoBehaviour
     private Collider2D collider;
     private Rigidbody2D rigidbody;
 
+    private float pastVelocity = 0;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.enabled && !audioSource.isPlaying)
         {
-            float velocityAtPoint = rigidbody.GetPointVelocity(collision.GetContact(0).point).magnitude * collisionVelocityMultiplier;
+            float velocityAtPoint = rigidbody.GetPointVelocity(collision.GetContact(0).point).magnitude - rigidbody.velocity.magnitude + pastVelocity;
             float audioSourceVolume = -Mathf.Pow(2, -velocityAtPoint) + 1;
             audioSource.volume = audioSourceVolume;
             audioSource.Play();
@@ -29,5 +31,15 @@ public class MakeNoiseOnCollision : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         collider = GetComponentInParent<Collider2D>();
         rigidbody = collider.attachedRigidbody;
+    }
+
+    private IEnumerator UpdatePastVelocity(int fixedUpdatesToWait)
+    {
+        float storedVelocity = rigidbody.velocity.magnitude;
+        for (int i = 0; i < fixedUpdatesToWait; i++)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        pastVelocity = storedVelocity;
     }
 }
