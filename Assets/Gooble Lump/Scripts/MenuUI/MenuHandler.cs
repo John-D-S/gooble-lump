f5a48af1785a9d173b0a7a003bdd7f9fed21b430
John-D-S <68535837+John-D-S@users.noreply.github.com>
2021-06-24 10:02:36 +1000

    deleted highscores and prepared project for building

--- Assets/Gooble Lump/Scripts/MenuUI/MenuHandler.cs
+++ Assets/Gooble Lump/Scripts/MenuUI/MenuHandler.cs
@@ -265,7 +265,9 @@ namespace Menu
             else
             {
                 Application.Quit();
+#if UNITY_EDITOR
                 UnityEditor.EditorApplication.isPlaying = false;
+#endif
             }
         }
 
